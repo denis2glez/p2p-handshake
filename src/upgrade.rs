ad4179e432e50b1744de0b0935a285d455ca33fa
Denis Deniz <denis2.glez@gmail.com>
2023-11-15 19:44:10 +0100

    feat: introduce the `SecurityUpgrade` trait
    Introduce the `SecurityUpgrade` trait as replacement for
    `InboundUpgrade` / `OutboundUpgrade`.

--- src/upgrade.rs
+++ src/upgrade.rs
@@ -2,22 +2,43 @@
 //!
 //! The `UpgradeInfo::protocol_info` method is called to determine which protocols are supported by
 //! the trait implementation.
-//! After a successful negotiation, the `InboundUpgrade::upgrade_inbound` or
-//! `OutboundUpgrade::upgrade_outbound`method is called. This method will return a `Future` that
-//! performs a handshake. This handshake is considered mandatory, however in practice it is possible
-//! for the trait implementation to return a dummy `Future`.
+//! After a successful negotiation, the `SecurityUpgrade::upgrade_security` method is called. This
+//! method will return a `Future` that performs a handshake. This handshake is considered mandatory,
+//! however in practice it is possible for the trait implementation to return a dummy `Future`.
 
-use futures::{future::BoxFuture, AsyncRead, AsyncWrite};
+use futures::{future::BoxFuture, AsyncRead, AsyncWrite, Future};
 use futures_rustls::TlsStream;
-use libp2p_core::upgrade::{InboundConnectionUpgrade, OutboundConnectionUpgrade, UpgradeInfo};
+use libp2p_core::upgrade::UpgradeInfo;
 use libp2p_identity::PeerId;
+use rustls::{ClientConfig, ServerConfig};
 use std::iter::{once, Once};
 
-#[derive(Clone)]
-pub struct Config {}
+use crate::error::TlsUpgradeError;
 
-#[derive(Debug)]
-pub enum UpgradeError {}
+/// Possible security upgrade on a connection
+pub trait SecurityUpgrade<T>: UpgradeInfo {
+    /// Output after the upgrade has been successfully negotiated and the handshake performed.
+    type Output;
+    /// Possible error during the handshake.
+    type Error;
+    /// Future that performs the handshake with the remote.
+    type Future: Future<Output = Result<Self::Output, Self::Error>>;
+
+    /// After we have determined that the remote supports one of the protocols we support, this
+    /// method is called to start the handshake.
+    ///
+    /// The `info` is the identifier of the protocol, as produced by `protocol_info`. Security
+    /// transports use the optional `peer_id` parameter on outgoing upgrades to validate the
+    /// expected `PeerId`.
+    fn upgrade_security(self, socket: T, info: Self::Info, peer_id: Option<PeerId>)
+        -> Self::Future;
+}
+
+#[derive(Clone)]
+pub struct Config {
+    server: ServerConfig,
+    client: ClientConfig,
+}
 
 impl UpgradeInfo for Config {
     type Info = &'static str;
@@ -28,28 +49,20 @@ impl UpgradeInfo for Config {
     }
 }
 
-impl<C> InboundConnectionUpgrade<C> for Config
+impl<C> SecurityUpgrade<C> for Config
 where
     C: AsyncRead + AsyncWrite + Send + Unpin + 'static,
 {
     type Output = (PeerId, TlsStream<C>);
-    type Error = UpgradeError;
+    type Error = TlsUpgradeError;
     type Future = BoxFuture<'static, Result<Self::Output, Self::Error>>;
 
-    fn upgrade_inbound(self, _socket: C, _: Self::Info) -> Self::Future {
-        unimplemented!()
-    }
-}
-
-impl<C> OutboundConnectionUpgrade<C> for Config
-where
-    C: AsyncRead + AsyncWrite + Send + Unpin + 'static,
-{
-    type Output = (PeerId, TlsStream<C>);
-    type Error = UpgradeError;
-    type Future = BoxFuture<'static, Result<Self::Output, Self::Error>>;
-
-    fn upgrade_outbound(self, _socket: C, _: Self::Info) -> Self::Future {
+    fn upgrade_security(
+        self,
+        socket: C,
+        info: Self::Info,
+        peer_id: Option<PeerId>,
+    ) -> Self::Future {
         unimplemented!()
     }
 }
